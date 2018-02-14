417acbeb398122dc6130fd4f72cabe6c67dfe869
schatell <sebastien.chatelle.blanchard@gmail.com>
2018-02-14 16:03:52 -0500

    SECRET

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJ6UN7VXWGZ6GLN2Q'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['myuniquerailstutorial']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
